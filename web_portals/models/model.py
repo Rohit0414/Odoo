167f792dbd392a79ad05cb9b92a16be1577f6877
Rohit <dhimanrohit070@gmail.com>
2025-03-09 23:06:04 +0530

    create-date, write-date intigration

--- web_portals/models/model.py
+++ web_portals/models/model.py
@@ -2,7 +2,7 @@ from odoo import fields, models
 
 class WebPortals(models.Model):
     _name='web.portals'
-    _description='that serve as gateways to a range of services (often requiring login) and provide a variety of information in one place.'
+    _description='that serve as gateways to a range of  (often requiring login) and provide a variety of information in one place.'
     
     
     name=fields.Char(string="Name", required=True)
